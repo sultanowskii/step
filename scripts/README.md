c32dca8fc3d5a97094ed09dea637b54b50f4ef6c
sultanowskii <dsr431@yandex.ru>
2024-07-06 19:13:39 +0300

    move scripts to dedicated folder

---
+++ scripts/README.md
@@ -0,0 +1,3 @@
+# scripts directory
+
+Before executing any script here, make sure you're doing it from project root!
--- scripts/valgrind_docker_run.sh
+++ scripts/valgrind_docker_run.sh
@@ -4,4 +4,4 @@
 # - https://github.com/karekoho/valgrind-container
 # - https://stackoverflow.com/questions/75292406/memchk-valgrind-reporting-inconsistent-results-in-different-docker-hosts
 
-docker run --rm -tiv ./:/valgrind karek/valgrind:latest bash -c "ulimit -n 1024 && ./valgrind_run.sh"
+docker run --rm -tiv ./:/valgrind karek/valgrind:latest bash -c "ulimit -n 1024 && ./scripts/valgrind_run.sh"
