e352f42ba7f0591ed0045c3e368cef365a880d81
sultanowskii <dsr431@yandex.ru>
2024-07-31 19:35:20 +0300

    add comments in fmt.h

--- src/fmt.h
+++ src/fmt.h
@@ -2,6 +2,9 @@
 
 #include <stddef.h>
 
+// Prints indent of the given size.
 void print_indent(size_t size);
+// Same as printf, but prints indent before.
 void iprintf(size_t indent_size, const char *format, ...);
+// Prints a newline.
 void print_newline();
